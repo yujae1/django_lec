11d2c4679fd2ad10c49f3ab83347938f63e24376
Seungjae Yu <dbtmdwo153@naver.com>
2023-12-01 02:58:35 +0900

    login,signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,15 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from.import views
+from . import views
 
 app_name = 'accounts'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
-
 ]
--- mysite/models.py
+++ mysite/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,3 +7,10 @@ class MainContent(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     pub_date = models.DateTimeField('date.published')
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    content_list = models.ForeignKey(MainContent, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    modify_date = models.DateTimeField(auto_now=True)
